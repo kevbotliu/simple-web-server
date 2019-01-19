7a9740561b06eaef1f2528037a10ad0370752e8f
Bradley Zhu <bzhu@cs130.org>
2019-01-19 12:07:53 +0000

    not working yet but going to sleep
    Change-Id: Icc9df21a386e28fad32fbdce544dbea5a4c57b34

---
+++ src/main.cc
@@ -0,0 +1,29 @@
+#include "server.h"
+#include "session.h"
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    using namespace std; // For atoi.
+    server s(io_service, atoi(argv[1]));
+
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
