1e132b81f743811c7bdb9a9c3975fe1f1f7bca59
Devel User <kliu@cs130.org>
2019-01-23 02:21:36 +0000

    Fix async write not flushing invalid requests
    Change-Id: Ic94e6bf7fb7c478e73cbe4901ea39b32aa5d44bb

--- src/session.cc
+++ src/session.cc
@@ -25,29 +25,27 @@ void session::handle_read(const boost::system::error_code& error,
       size_t bytes_transferred)
   {
     if (!error)
-    {
-      // printf("%s", data_);
-      
-      std::vector<std::string> segs;
-      boost::split(segs, data_, boost::is_any_of("\r\n"));
-      if (segs.size() > 1) {
+    { 
+      std::string body = data_;
+      std::string response = "";
+      std::vector<std::string> lines;
+      boost::split(lines, body, boost::is_any_of("\r\n"));
+      if (lines.size() >= 1) {
         std::vector<std::string> statuses;
-        boost::split(statuses, segs[0], boost::is_any_of(" "));
+        boost::split(statuses, lines[0], boost::is_any_of(" "));
 
-        if (boost::iequals(statuses[0], "GET") && boost::iequals(statuses[2], "HTTP/1.1")) {
+        if (statuses.size() == 3 && boost::iequals(statuses[0], "GET") && boost::iequals(statuses[2], "HTTP/1.1")) {
           std::string status_line = "HTTP/1.1 200 OK\r\n";
           std::string content_type = "Content-Type: text/plain\r\n";
           std::string headers = status_line + content_type + "\r\n";
 
-          std::string body = data_;
-          std::string response = headers + body;
-
-          boost::asio::async_write(socket_,
-              boost::asio::buffer(response, response.size()),
-              boost::bind(&session::handle_write, this,
-                boost::asio::placeholders::error));
+          response = headers + body;
         }
       }
+      boost::asio::async_write(socket_,
+          boost::asio::buffer(response, response.size()),
+          boost::bind(&session::handle_write, this,
+            boost::asio::placeholders::error));
     }
     else
     {
