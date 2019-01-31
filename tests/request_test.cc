08fd16f46b32df647312ec15a45752d2ca11e24e
Brian You <brianyou97@cs130.org>
2019-01-31 07:43:06 +0000

    Made test cases for request.cc and response.cc
    Change-Id: I894a0b761483528eb58c70ab8fda8328bdd880d6

---
+++ tests/request_test.cc
@@ -0,0 +1,44 @@
+#include "gtest/gtest.h"
+#include "request.h"
+
+class RequestTest : public ::testing::Test {
+  protected:
+    void SetUp() override {}
+    void TearDown() {}
+
+    bool success;
+};
+
+
+
+TEST_F(RequestTest, HeaderTest) {
+
+  const std::string request = "GET / HTTP/1.1\r\nHeader: Example\r\n\r\n";
+  Request *k = new Request(request);
+  success = k->is_valid_syntax();
+  EXPECT_TRUE(success);
+
+}
+
+
+TEST_F(RequestTest, pathTest) {
+  const std::string request = "GET /HTTP /1.1\r\n\r\n";
+  Request *k = new Request(request);  
+  std::string path = k->get_path();
+  EXPECT_TRUE(path.compare("/HTTP") == 0);
+  
+}
+
+TEST_F(RequestTest, getHeaderTest) {
+
+  const std::string request = "GET / HTTP/1.1\r\nHeader: Example\r\n\r\n";
+  Request *k = new Request(request);
+  std::vector<std::pair<std::string, std::string>> headers = k->get_headers();
+  std::pair<std::string, std::string> theHeaderWeMade = headers.at(0);
+  std::string headerName = theHeaderWeMade.first;
+  std::string headerValue = theHeaderWeMade.second;
+
+  EXPECT_TRUE(headerName.compare("Header") == 0 && headerValue.compare("Example") == 0);
+
+}
+
