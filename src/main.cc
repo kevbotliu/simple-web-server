1e4778434f98bb417cc232b10a076cfba50d3d72
Brian You <brianyou97@cs130.org>
2019-02-07 06:14:37 +0000

    Add log cycling.
    Change-Id: I49c270d2f735bb0c8cf9728a8d2079fe43cf5ad9

--- src/main.cc
+++ src/main.cc
@@ -3,11 +3,41 @@
 #include <cstdlib>
 #include <iostream>
 
+// Boost libraries
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/trivial.hpp>
+
+void init_logging() {
+  // Enable logging to file
+  auto s = boost::log::add_file_log(
+    // File name
+    boost::log::keywords::file_name = "main.log",
+    // Flush (write to file) immediately after error message
+    boost::log::keywords::auto_flush = true
+    // Midnight rotation
+    boost::log::keywords::time_based_rotation = sinks::file::rotation_at_time_point(0,0,0),
+    // 10MB rotation
+    boost::log::keywords::rotationIsze = 10 * 1024 * 1024,
+  );
+  // Enable logging to console
+  boost::log::add_console_log(); 
+
+  boost::log::add_common_attributes();
+
+}
 
 int main(int argc, char* argv[])
 {
-  BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+  init_logging();
+
+    BOOST_LOG_TRIVIAL(trace) << "This is a trace severity message";
+    BOOST_LOG_TRIVIAL(debug) << "This is a debug severity message";
+    BOOST_LOG_TRIVIAL(info) << "This is an informational severity message"; 
+    BOOST_LOG_TRIVIAL(warning) << "This is a warning severity message";
+    BOOST_LOG_TRIVIAL(error) << "This is an error severity message";
+    BOOST_LOG_TRIVIAL(fatal) << "and this is a fatal severity message";
 
   try
   {
