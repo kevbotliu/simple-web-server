77a8b77106a4242c41ec3fd0bf183217ab7a54a3
Bradley Zhu <bzhu@cs130.org>
2019-02-21 00:46:15 +0000

    static API
    Change-Id: I014957cea3111f504fecaff6b57190475123b41a

--- include/static_handler.h
+++ include/static_handler.h
@@ -1,20 +1,20 @@
-// #ifndef STATIC_HANDLER_H
-// #define STATIC_HANDLER_H
+#ifndef STATIC_HANDLER_H
+#define STATIC_HANDLER_H
 
-// #include "request_handler.h"
+#include "request_handler.h"
 
-// class StaticHandler : public RequestHandler
-// {
-// public:
-// 	StaticHandler(Request *req, Response *resp, std::string root_dir);
+class StaticHandler : public RequestHandler {
+public:
+	static RequestHandler* create(const NginxConfig& config, const std::string& root_path);
 
-// 	bool process() override;
-// 	bool build_response() override;
+	std::unique_ptr<Reply> HandleRequest(const Request& request) override;
 
-// private:
-// 	std::string ext;
-	
-// 	void setExtension(std::string path);
-// 	std::string findFullPath(std::string path);
-// };
-// #endif
+private:
+	std::string ext;
+	StaticHandler(std::string root_path);
+
+	void setExtension(std::string path);
+	std::string findFullPath(std::string path);
+	ReplyArgs build_response(const Request& request);
+};
+#endif
--- src/static_handler.cc
+++ src/static_handler.cc
@@ -1,91 +1,100 @@
-// #include "static_handler.h"
-// #include "request.h"
-// #include "reply.h"
-// #include <string>
-// #include <fstream>
-// #include <sstream>
-// #include <iostream>
+#include "static_handler.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 
-// StaticHandler::StaticHandler(Request *req, Response *resp, std::string root_dir)
-// 	: RequestHandler(req, resp, root_dir)
-// {
-// 	succeeded_ = process();
-// }
+RequestHandler* StaticHandler::create(const NginxConfig& config, const std::string& root_path)
+{
+	return new StaticHandler(root_path);
+}
 
+StaticHandler::StaticHandler(std::string root_path) {
+	root_path_ = root_path;
+}
 
-// bool StaticHandler::process()
-// {
-// 	if ( 
-// 		req_->is_valid_syntax()
-// 		&& req_->get_method() == "GET"
-// 		&& req_->get_version() == "HTTP/1.1"
-// 	) {
-// 		build_response();
-// 		return true;
-// 	}
-// 	else resp_->invalidate();
+std::unique_ptr<Reply> StaticHandler::HandleRequest(const Request& request) {
+	if (!request.is_valid()) {
+		return std::unique_ptr<Reply>(nullptr);
+	}
+
+	ReplyArgs args;
+	if (request.get_method() != "GET") {
+		return std::unique_ptr<Reply>(nullptr);
+	}
+
+	args = build_response(request);
+
+	return std::unique_ptr<Reply>(new Reply(args));
 	
-// 	return false;
-// }
+}
 
-// bool StaticHandler::build_response()
-// {
-// 	std::string path = req_->get_path();
-// 	setExtension(path);
+ReplyArgs StaticHandler::build_response(const Request& request)
+{
+	ReplyArgs args;
+	std::string path = request.get_path();
+	setExtension(path);
 
-// 	resp_->set_version(req_->get_version());
-// 	resp_->set_status(200);
-// 	std::pair<std::string, std::string> header = std::make_pair("Content-type", ext);
-// 	resp_->add_header(header);
+	// resp_->set_version(req_->get_version());
+	// resp_->set_status(200);
+	// std::pair<std::string, std::string> header = std::make_pair("Content-type", ext);
+	// resp_->add_header(header);
+
+	std::pair<std::string, std::string> header = std::make_pair("Content-type", ext);
+	args.headers.push_back(header);
+
+	std::string full_path = findFullPath(path);
+	std::ifstream f(full_path.c_str(), std::ios::in|std::ios::binary|std::ios::ate);
+	if (f.is_open()) {
+		std::streampos mSize = f.tellg();
+		char* mBuffer = new char[mSize];
+		f.seekg(0, std::ios::beg);
+		f.read(mBuffer, mSize);
+		f.close();
+
+		// resp_->set_version(req_->get_version());
+		// resp_->set_status(200);
+		// std::pair<std::string, std::string> header = std::make_pair("Content-type", ext);
+		// resp_->add_header(header);
+		// resp_->set_body(std::string(mBuffer, mSize));
+		args.body = std::string(mBuffer, mSize);
+		delete mBuffer;
+		return args;
+	} else {
+		// resp_->set_version(req_->get_version());
+		// resp_->set_status(404);
+		// std::pair<std::string, std::string> header = std::make_pair("Content-type", "text/html");
+		// resp_->add_header(header);
+		args.body = std::string("404 Not Found");
+		std::cerr << "Could not open file.";
+		return args;
+	}
+	return args;
+}
+
+std::string StaticHandler::findFullPath(std::string path) {
+	///static/file.html means you want the 7th character on
+	//std::cerr << path.substr(7);
+	//TODO: configure the home directory
+	// std::cerr << "/usr/src/project/" + root_path_ + path.substr(7);
+	// return "/usr/src/project/" + root_path_ + path.substr(7);
+	return "/usr/src/projects/bbk-simple-echo-server/" + root_path_ + path.substr(7);
 	
-// 	std::string full_path = findFullPath(path);
-// 	std::ifstream f(full_path.c_str(), std::ios::in|std::ios::binary|std::ios::ate);
-// 	if (f.is_open()) {
-// 		std::streampos mSize = f.tellg();
-// 		char* mBuffer = new char[mSize];
-// 		f.seekg(0, std::ios::beg);
-// 		f.read(mBuffer, mSize);
-// 		f.close();
+}
 
-// 		resp_->set_version(req_->get_version());
-// 		resp_->set_status(200);
-// 		std::pair<std::string, std::string> header = std::make_pair("Content-type", ext);
-// 		resp_->add_header(header);
-// 		resp_->set_body(std::string(mBuffer, mSize));
-// 		delete mBuffer;
-// 		return true;
-// 	} else {
-// 		resp_->set_version(req_->get_version());
-// 		resp_->set_status(404);
-// 		std::pair<std::string, std::string> header = std::make_pair("Content-type", "text/html");
-// 		resp_->add_header(header);
-// 		resp_->set_body("404 Not Found");
-// 		std::cerr << "Could not open file.";
-// 		return false;
-// 	}
-// 	return false;
-// }
+void StaticHandler::setExtension(std::string path)
+{
+	int mIndex = path.find_last_of(".");
+	std::string mExtension = path.substr(mIndex + 1);
 
-// std::string StaticHandler::findFullPath(std::string path) {
-// 	///static/file.html means you want the 7th character on
-// 	//std::cerr << path.substr(7);
-// 	//TODO: configure the home directory
-// 	return "/usr/src/project/" + root_dir_ + path.substr(7);
-// }
-
-// void StaticHandler::setExtension(std::string path)
-// {
-// 	int mIndex = path.find_last_of(".");
-// 	std::string mExtension = path.substr(mIndex + 1);
-
-// 	if (mExtension == "html") {
-// 		ext = "text/html";
-// 	} else if (mExtension == "jpg") {
-// 		ext = "image/jpeg";
-// 	} else if (mExtension == "zip") {
-// 		ext = "application/zip";
-// 	} else if (mExtension == "txt") {
-// 		ext = "text/plain";
-// 	}
-// }
+	if (mExtension == "html") {
+		ext = "text/html";
+	} else if (mExtension == "jpg") {
+		ext = "image/jpeg";
+	} else if (mExtension == "zip") {
+		ext = "application/zip";
+	} else if (mExtension == "txt") {
+		ext = "text/plain";
+	}
+}
