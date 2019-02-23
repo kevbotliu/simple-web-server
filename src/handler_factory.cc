14078cd71491fdf9ccafde1f894f31969a150e73
Victor Fu <victorfu@cs130.org>
2019-02-23 06:08:56 +0000

    Added proxy to the handler factory
    Change-Id: Ieed5e360cfdcb4e1a1b09a86e33ccdb0ed90f079

--- src/handler_factory.cc
+++ src/handler_factory.cc
@@ -3,13 +3,15 @@
 #include "static_handler.h"
 #include "status_handler.h"
 #include "not_found_handler.h"
+#include "proxy_handler.h"
 #include "pathfinder.h"
 
 std::unique_ptr<RequestHandler> HandlerFactory::createByName(const std::string& name, const NginxConfig& config, const std::string& root_path) {
     if (name == "echo") return std::unique_ptr<RequestHandler>(EchoHandler::create(config, root_path));
     if (name == "static") return std::unique_ptr<RequestHandler>(StaticHandler::create(config, root_path));
     if (name == "status") return std::unique_ptr<RequestHandler>(StatusHandler::create(config, root_path));
-    
+    if (name == "proxy") return std::unique_ptr<RequestHandler>(ProxyHandler::create(config, root_path));
+
     return std::unique_ptr<RequestHandler>(NotFoundHandler::create(config, root_path));
 }
 
