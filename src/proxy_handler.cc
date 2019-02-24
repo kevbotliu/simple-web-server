ec3e101917aaa1354cf883d2a292d6cb0b895fcb
Konner <konnermacias@cs130.org>
2019-02-24 05:56:56 +0000

    getting some content back for proxy handler now
    Change-Id: Id2684bc91913dcd81c9eb51e94f145b5efcc2de9

--- src/proxy_handler.cc
+++ src/proxy_handler.cc
@@ -18,16 +18,15 @@
 using boost::asio::ip::tcp;
 
 RequestHandler* ProxyHandler::create(const NginxConfig& config, const std::string& root_path) {
-	// TODO: would like config to have method: getAttribute("remote_url") and "remote_port"
 	return new ProxyHandler();
 }
-
 
 std::unique_ptr<Reply> ProxyHandler::HandleRequest(const Request& request) {
 	/*
 		Author: Konner Macias
 
-		Checks for valid
+		Checks for valid request, creates new session, sends out response
+		to new remote url and port. Get's responsea and checks is complete.
 	*/
 	if (!request.is_valid()) return std::unique_ptr<Reply>(nullptr);
 	if (request.get_method() != "GET") return std::unique_ptr<Reply>(nullptr);
@@ -36,8 +35,6 @@ std::unique_ptr<Reply> ProxyHandler::HandleRequest(const Request& request) {
 	boost::asio::io_service io_service;
 	tcp::resolver resolver(io_service);
 
-	// TODO: this is getting a HOST NOT FOUND (authoritative) error
-
 	// obtains pair (remote_url, remote_port)
 	std::pair<std::string,std::string> remote_pair = get_remote_info();
 	std::string host = remote_pair.first, port = remote_pair.second;
@@ -45,16 +42,14 @@ std::unique_ptr<Reply> ProxyHandler::HandleRequest(const Request& request) {
 	// send out query
 	std::cout << "Host: " << host << " Port: " << port << std::endl;
 	tcp::resolver::query query(host, port, boost::asio::ip::resolver_query_base::numeric_service);
-	
 	tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
 	tcp::resolver::iterator iter = endpoint_iterator;
 	tcp::resolver::iterator end;
 
 	// iterate until endpoint
 	while (iter != end) {
 		tcp::endpoint endpoint = *iter++;
-		std::cout << endpoint << std::endl;
+		std::cout << "Endpoint: " << endpoint << std::endl;
 	}
 	
 	// get session/connection
@@ -72,72 +67,79 @@ std::unique_ptr<Reply> ProxyHandler::HandleRequest(const Request& request) {
 	// send out built request
 	socket.write_some(boost::asio::buffer(request_str, request_str.size()));
 
+	std::cout << "Successfully wrote data" << std::endl;
+
 	// read in response data
-	boost::array<char, 2048> buffy;
-	boost::system::error_code error;
-	size_t len_data = socket.read_some(boost::asio::buffer(buffy), error);
-
-	// check for EOF
-	if (error == boost::asio::error::eof) {
-		BOOST_LOG_TRIVIAL(trace) << "EOF reached!";
-		// HEY: do I throw a nullptr here?
-		return std::unique_ptr<Reply>(nullptr);
-	}
-
-	// assign response string to incoming data
 	std::string returned_response;
-	returned_response += std::string(buffy.data(), len_data);
+	while(1) {
+		boost::array<char, 4096> buffy;
+		boost::system::error_code error;
+		size_t len_data = socket.read_some(boost::asio::buffer(buffy), error);
+		
+		// check for EOF
+		if (error == boost::asio::error::eof) {
+			BOOST_LOG_TRIVIAL(trace) << "EOF reached!";
+			break;
+		}
 
-	// define a function to check for invalid response (use if switch to - vars)
-	/*
-	bool returned_response_check;
-	if (!(returned_response_check = parse_returned_response(returned_response))) {
-		return std::unique_ptr<Reply>(nullptr);
+		// assign response string to incoming data
+		returned_response += std::string(buffy.data(), len_data);
 	}
-	*/
 
-	// get pair of (whether it's valid, body of response)
-	// -> doing this bc don't wanna use private variables
-	std::pair<bool,std::string> parse_pair = parse_returned_response(returned_response);
+	std::cout << "Received response string, parsing..." << std::endl;
+
+	// get the response in parsed format
+	std::map<std::string,std::string> parse_map = parse_returned_response(returned_response);
 
 	// check for invalid response
-	if (!parse_pair.first) {
+	if (parse_map["Valid"] == "false") {
 		return std::unique_ptr<Reply>(nullptr);
 	}
-	std::string response_body = parse_pair.second;
+	std::string response_body = parse_map["Body"];
 
 	// complete final response
 	ReplyArgs args;
-	args.headers.push_back(std::make_pair("Content-type", "text/plain"));
+	args.headers.push_back(std::make_pair("Content-type", "text/html"));
+	args.headers.push_back(std::make_pair("Content-length", std::to_string(response_body.length())));
 	args.body = response_body;
+	std::cout << "Completed building final response" << std::endl;
 
 	return std::unique_ptr<Reply>(new Reply(args));
 }
 
 
-std::pair<bool,std::string> ProxyHandler::parse_returned_response(std::string response) {
+std::map<std::string,std::string> ProxyHandler::parse_returned_response(std::string response) {
 	/*
 		Author: Konner Macias
 
-		Parses and Confirms a response in valid.
+		Parses and Confirms a response in valid. Returns a map with possible data members:
+			- Valid
+			- Status
+			- Headers
+			- Body
 	*/
+	std::map<std::string,std::string> response_map;
+	response_map["Valid"] = "true";
+
 	std::size_t status_index;
 	if ((status_index = response.find("\r\n")) == std::string::npos) {
-		return std::pair<bool,std::string> (false, "");
+		response_map["Valid"] = "false";
+		return response_map;
 	}
-
-	std::string remainder = response.substr(0, status_index);
 
 	std::size_t header_index;
-	if ((header_index = remainder.find("\r\n\r\n")) == std::string::npos) {
-		return std::pair<bool,std::string> (false, "");
+	if ((header_index = response.find("\r\n\r\n")) == std::string::npos) {
+		response_map["Valid"] = "false";
+		return response_map;
 	}
 
-	std::string response_status = response.substr(0, status_index + 3);
-	std::string response_body = response.substr(0, header_index + 4);
+	// TODO: got some magic numbers here
+	response_map["Status"] = response.substr(0, status_index + 1);
+	response_map["Headers"] = response.substr(0, header_index);
+	response_map["Body"] = response.substr(header_index + 11, response.size());
 
-	// TODO: add checks for validity on these strings, continue on
-	return std::pair<bool,std::string> (true, response_body);
+	// TODO: add checks further checks for validity on these strings, continue on
+	return response_map;
 }
 
 std::pair<std::string,std::string> ProxyHandler::get_remote_info() {
