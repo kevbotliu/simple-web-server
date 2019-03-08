b6b0f451db5d02c26f4f38c0dcea5674a214d519
brian <brianyou97@cs130.org>
2019-03-08 03:38:12 +0000

    Fix missing image issue, add new meme handler
    Change-Id: I97895b505ab1748f286d8a795ab4d186700c1d74

--- src/meme_handler.cc
+++ src/meme_handler.cc
@@ -14,8 +14,9 @@ std::unique_ptr<Reply> MemeHandler::HandleRequest(const Request& request) {
 	std::string subpath = request.get_path().erase(0, 5);
 
 	if (request.get_method() == "GET") {
+		// Personally I believe this should be the default
 		if (subpath.find("/new") == 0) {
-			// return handleNew...
+			return handleNew();
 		}
 		if (subpath.find("/view") == 0) {
 			if (subpath.size() > 9 && subpath.find("?id=") == 5) {
@@ -31,13 +32,30 @@ std::unique_ptr<Reply> MemeHandler::HandleRequest(const Request& request) {
 
 	if (request.get_method() == "POST") {
 		if (subpath.find("/create") == 0) {
-			// return handleCreate...
+			// Do this...
 		}
 	}
 
 	return std::unique_ptr<Reply>(new Reply(false));
 }
 
+// Handle creating memes
+std::unique_ptr<Reply> MemeHandler::handleNew() {
+	
+	std::string filepath = "../" + root_path_ + "/" + "memeform.html";
+
+	ReplyArgs args;
+	args.headers.push_back(std::make_pair("Content-type", "text/html"));
+
+	// Credit: https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+	std::ifstream t(filepath);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	args.body = buffer.str();
+
+	return std::unique_ptr<Reply>(new Reply(args));
+
+}
 // Handle viewing individual memes.
 std::unique_ptr<Reply> MemeHandler::handleView(int id) {
 	NginxConfig meme_info;
@@ -98,7 +116,7 @@ std::unique_ptr<Reply> MemeHandler::handleView(int id) {
 	std::string page_styles = "<style> body {display: flex; justify-content: center; position: relative;} span {color: black; font: 4em bold; text-transform: uppercase; font-family: 'Oswald', sans-serif; sans-serif; position: absolute; text-align: center; width: 100%;} #top {top: 0;} #bottom {bottom: 0;}</style>";
 	std::string page_body = "<body>";
 	// page_body += "<img src=\"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png\">";
-	page_body += "<img src=\"../" + root_path_ + "/" + image_path + "\">";
+	page_body += "<img src=\"../static/" + image_path + "\">";
 	page_body += "<span id=\"top\">" + top_text +"</span>";
 	page_body += "<span id=\"bottom\">" + bottom_text + "</span></body>";
 
