e2e6048296ad8fd88009ae09a55a8018b72fd508
Devel User <kliu@cs130.org>
2019-03-09 05:51:05 +0000

    Fix static handler cloud build bug
    Change-Id: I5b9f4071a4cc9368b35fdc0e39deb68967160249

--- tests/static_handler_test.cc
+++ tests/static_handler_test.cc
@@ -2,51 +2,46 @@
 #include "request.h"
 #include "reply.h"
 #include "static_handler.h"
+#include "config_parser.h"
 
 class StaticHandlerTest : public ::testing::Test {
   protected:
-    void SetUp() override {}
+    void SetUp() override {
+        NginxConfigParser parser;
+        assert(parser.Parse("../server_config_cloud", &config));
+        config.extractHandlerInfo();
+    }
     void TearDown() {}
 
+    NginxConfig config;
+    std::string root_path = "static_src";
     bool success;
 };
 
 TEST_F(StaticHandlerTest, ValidResponseTest) {
-    success = true;
     std::string s = "GET /static/blah.txt HTTP/1.1\r\n\r\n";
     const Request *req = new Request(s);
-    NginxConfig config;
-    std::string root_path = "static_src";
     success = StaticHandler::create(config, root_path)->HandleRequest(*req)->is_valid();
     EXPECT_TRUE(success);
 }
 
 TEST_F(StaticHandlerTest, StatusCodeResponseTest) {
-    success = true;
     std::string s = "GET /static/blah.txt HTTP/1.1\r\n\r\n";
     const Request *req = new Request(s);
-    NginxConfig config;
-    std::string root_path = "static_src";
     success = (StaticHandler::create(config, root_path)->HandleRequest(*req)->get_status_code()==200);
     EXPECT_TRUE(success);
 }
 
 TEST_F(StaticHandlerTest, NotFoundResponseTest) {
-    success = true;
     std::string s = "GET /static/bla.txt HTTP/1.1\r\n\r\n";
     const Request *req = new Request(s);
-    NginxConfig config;
-    std::string root_path = "static_src";
     success = (StaticHandler::create(config, root_path)->HandleRequest(*req)->get_status_code()==404);
     EXPECT_TRUE(success);
 }
 
 TEST_F(StaticHandlerTest, CorrectResponseTest) {
-    success = true;
     std::string s = "GET /static/blah.txt HTTP/1.1\r\n\r\n";
     const Request *req = new Request(s);
-    NginxConfig config;
-    std::string root_path = "static_src";
     success = (StaticHandler::create(config, root_path)->HandleRequest(*req)->get_body()=="words words words newline\nmore words\n2\nnewlines\nend");
     EXPECT_TRUE(success);
 }
