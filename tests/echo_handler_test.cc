95c818c90822b6cd67debbce19a0175b32ff4a6e
Andrew Xue <axue@cs130.org>
2019-03-11 21:15:06 +0000

    Bugfix: library dependency and unit tests
    Change-Id: I57aea7ab32f22f240c52de623597e39cab6aa615

--- tests/echo_handler_test.cc
+++ tests/echo_handler_test.cc
@@ -1,42 +1,42 @@
-// #include "gtest/gtest.h"
-// #include "echo_handler.h"
-// #include "request.h"
-// #include "reply.h"
+#include "gtest/gtest.h"
+#include "echo_handler.h"
+#include "request.h"
+#include "reply.h"
 
-// class EchoHandlerTest : public ::testing::Test {
-//   protected:
-//     void SetUp() override {}
-//     void TearDown() {}
+class EchoHandlerTest : public ::testing::Test {
+  protected:
+    void SetUp() override {}
+    void TearDown() {}
 
-//     bool success;
-// };
+    bool success;
+};
 
-// TEST_F(EchoHandlerTest, ValidResponseTest) {
-//     success = true;
-//     std::string s = "GET /echo HTTP/1.1\r\n\r\n";
-//     const Request *req = new Request(s);
-//     NginxConfig config;
-//     std::string root_path = "echo";
-//     success = EchoHandler::create(config, root_path)->HandleRequest(*req)->is_valid();
-//     EXPECT_TRUE(success);
-// }
+TEST_F(EchoHandlerTest, ValidResponseTest) {
+    success = true;
+    std::string s = "GET /echo HTTP/1.1\r\n\r\n";
+    const Request *req = new Request(s);
+    NginxConfig config;
+    std::string root_path = "echo";
+    success = EchoHandler::create(config, root_path)->HandleRequest(*req)->is_valid();
+    EXPECT_TRUE(success);
+}
 
-// TEST_F(EchoHandlerTest, StatusCodeResponseTest) {
-//     success = true;
-//     std::string s = "GET /echo HTTP/1.1\r\n\r\n";
-//     const Request *req = new Request(s);
-//     NginxConfig config;
-//     std::string root_path = "echo";
-//     success = (EchoHandler::create(config, root_path)->HandleRequest(*req)->get_status_code()==200);
-//     EXPECT_TRUE(success);
-// }
+TEST_F(EchoHandlerTest, StatusCodeResponseTest) {
+    success = true;
+    std::string s = "GET /echo HTTP/1.1\r\n\r\n";
+    const Request *req = new Request(s);
+    NginxConfig config;
+    std::string root_path = "echo";
+    success = (EchoHandler::create(config, root_path)->HandleRequest(*req)->get_status_code()==200);
+    EXPECT_TRUE(success);
+}
 
-// TEST_F(EchoHandlerTest, CorrectResponseTest) {
-//     success = true;
-//     std::string s = "GET /echo HTTP/1.1\r\n\r\n";
-//     const Request *req = new Request(s);
-//     NginxConfig config;
-//     std::string root_path = "echo";
-//     success = (EchoHandler::create(config, root_path)->HandleRequest(*req)->get_body().substr(0, 18)=="GET /echo HTTP/1.1");
-//     EXPECT_TRUE(success);
-// }
+TEST_F(EchoHandlerTest, CorrectResponseTest) {
+    success = true;
+    std::string s = "GET /echo HTTP/1.1\r\n\r\n";
+    const Request *req = new Request(s);
+    NginxConfig config;
+    std::string root_path = "echo";
+    success = (EchoHandler::create(config, root_path)->HandleRequest(*req)->get_body().substr(0, 18)=="GET /echo HTTP/1.1");
+    EXPECT_TRUE(success);
+}
